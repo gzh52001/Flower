c9789b73e85f08fb2dd6fd426bd2f63d596067f8
IT-965 <IT_node_vue_react@yeah.net>
2020-07-15 20:58:20 +0800

    1.0

---
+++ flower/src/pages/List/index.js
@@ -0,0 +1,14 @@
+import React,{component} from 'react';
+
+class List extends Component{
+    render(){
+
+        return (
+            <div>
+                List
+            </div>
+        )
+    }
+}
+
+export default Mine;
