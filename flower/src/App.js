4db9eb7fce2b61457142174412cb776a0ab3cf6b
IT-965 <IT_node_vue_react@yeah.net>
2020-07-16 20:35:57 +0800

    7-16

--- flower/src/App.js
+++ flower/src/App.js
@@ -1,17 +1,27 @@
 import React from 'react';
 import { Route, Redirect, Switch } from 'react-router-dom'
-import Mine from './pages/Mine/index'
 import Home from './pages/Home/index'
+import List from './pages/List/index'
+import Cart from './pages/Cart/index'
+import Mine from './pages/Mine/index'
 
 function App() {
   const menu = [
     {
-      text: '我的',
-      path: '/mine'
-    },
-    {
       text: '首页',
       path: '/home'
+    },
+    {
+      text:'分类',
+      path:'/list'
+    },
+    {
+      text:'购物车',
+      path:'/cart'
+    },
+    {
+      text: '我的',
+      path: '/mine'
     },
   ];
   // const goto = (path) => {
@@ -20,8 +30,10 @@ function App() {
   return (
     <div >
       <Switch>
-        <Route path='/mine' component={Mine} />
         <Route path='/home' component={Home} />
+        <Route path='/list' component={List} />
+        <Route path='/cart' component={Cart} />
+        <Route path='/mine' component={Mine} />
         <Redirect from='/' to='/home' exact />
       </Switch>
     </div>
