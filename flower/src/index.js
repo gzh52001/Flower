2f75e1df745fd66059a5c8a88b629660ba624704
IT-965 <IT_node_vue_react@yeah.net>
2020-07-16 19:55:45 +0800

    1.00

--- flower/src/index.js
+++ flower/src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import 'amfe-flexible'
 import { HashRouter, BrowserRouter, Route } from 'react-router-dom'
 
 const Router = process.env.NODE_ENV === 'production' ? BrowserRouter : HashRouter;
