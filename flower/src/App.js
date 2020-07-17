cc295d526ec542b4badacce5c0c1c6d385334504
IT-965 <IT_node_vue_react@yeah.net>
2020-07-17 21:02:24 +0800

    7-117

--- flower/src/App.js
+++ flower/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from 'react'
 import { Route, Redirect, Switch } from 'react-router-dom'
 import './base/base.css'
 import Mine from './pages/Mine/index'
@@ -10,35 +10,35 @@ function App() {
   const menu = [
     {
       text: '首页',
-      path: '/home'
+      path: '/home',
     },
     {
       text: '分类',
-      path: '/list'
+      path: '/list',
     },
     {
       text: '购物车',
-      path: '/cart'
+      path: '/cart',
     },
     {
       text: '我的',
-      path: '/mine'
+      path: '/mine',
     },
-  ];
+  ]
   // const goto = (path) => {
   //   props.history.push(path);
   // }
   return (
-    <div >
+    <div>
       <Switch>
-        <Route path='/home' component={Home} />
-        <Route path='/list' component={List} />
-        <Route path='/cart' component={Cart} />
-        <Route path='/mine' component={Mine} />
-        <Redirect from='/' to='/home' exact />
-      </Switch>
+        <Route path="/home" component={Home} />{' '}
+        <Route path="/list" component={List} />{' '}
+        <Route path="/cart" component={Cart} />{' '}
+        <Route path="/mine" component={Mine} />{' '}
+        <Redirect from="/" to="/home" exact />
+      </Switch>{' '}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- flower/src/pages/Home/index.js
+++ flower/src/pages/Home/index.js
@@ -1,13 +1,43 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+// import { Carousel } from 'antd'
+import Banner from './banner'
+import { BarsOutlined, CustomerServiceOutlined } from '@ant-design/icons'
+import './home.scss'
 
 class Home extends Component {
-    render() {
-
-        return (
-            <div>
-                123
+  render() {
+    return (
+      <div>
+        {/* {<!--HeaderBar-->} */}
+        <div className="headerbar">
+          <div className="headerbar-left">
+            <a href="">
+              <BarsOutlined className="iconfont iconfont-sevice" />
+            </a>
+          </div>
+          <div className="headerbar-logo">
+            <img
+              src="https://m.hua.com/content/vue/login/static/img/m_hualogo.png"
+              alt=""
+            />
+          </div>
+          <div className="headerbar-right">
+            <div className="headerbar-right-kf active">
+              <CustomerServiceOutlined className="iconfont iconfont-sevice" />
             </div>
-        )
-    }
+          </div>
+        </div>
+        {/* {Banner} */}
+        <Banner />
+        {/* ConpanyInfo */}
+        <div className="conpany-info">
+        <div className="conpany-info-item">认证龙头企业</div>
+        <div className="conpany-info-item">15年品牌</div>
+        <div className="conpany-info-item">3小时送花</div>
+        <div className="conpany-info-item">最近<a href="/productpj/"><u id="commentCount">375654</u></a>条好评</div>
+    </div>
+      </div>
+    )
+  }
 }
-export default Home;
+export default Home
