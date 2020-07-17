901a0a4ce9f15f9202fa7d79e415d28463d23a27
IT-965 <IT_node_vue_react@yeah.net>
2020-07-17 14:26:08 +0800

    7-17

--- flower/src/index.js
+++ flower/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import 'amfe-flexible';
+import 'lib-flexible'
 import { HashRouter, BrowserRouter, Route } from 'react-router-dom'
 
 const Router = process.env.NODE_ENV === 'production' ? BrowserRouter : HashRouter;
--- flower/src/pages/Home/index.js
+++ flower/src/pages/Home/index.js
@@ -1,11 +1,30 @@
 import React, { Component } from 'react';
+import './home.scss'
 
 class Home extends Component {
     render() {
 
         return (
             <div>
-                      123
+            <div className="headerbar">
+
+            <div className="headerbar-left">
+            <a href="">
+                <i className="iconfont iconfont-category"></i>
+            </a>
+                </div>
+
+            <div className="headerbar-logo">
+                 <img src="https://m.hua.com/content/vue/login/static/img/m_hualogo.png" alt=""/>
+            </div>
+
+            <div className="headerbar-reght">
+                <div className="headerbar-right-kf active">
+                    <p className="iconfont iconfont-sevice"></p>
+                </div>
+             </div>
+
+            </div>
             </div>
         )
     }
